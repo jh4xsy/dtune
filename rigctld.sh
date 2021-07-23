fc3a7bf60bf323c6ebeb8df2b3cb6081c41e930a
Hiroshi Iwamoto <sx7h-iwmt@asahi-net.or.jp>
2021-07-23 19:51:33 +0900

    Changed to IC-9700

--- rigctld.sh
+++ rigctld.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# IC-910 (CI-V Address:$4C) + RigBlaster Advantage
-rigctld -r /dev/ttyUSB0 -s 19200 -m 334 -t 4533 -C rts_state=OFF -C dtr_state=OFF &
+# IC-9700
+rigctld -r /dev/ic9700a -s 19200 -m 3081 -t 4533 &
